f31affcc26bd6a50c0439ef24061e02708002819
rory-reidy <roryreidy@gmail.com>
2021-12-25 20:27:52 -0600

    add file log

--- movscript.sh
+++ movscript.sh
@@ -1,4 +1,7 @@
-cd /media/irrational/3434-6632/
+cd ..
+#cd /media/irrational/3434-6632/
+
+find -name \*.WAV > script/fileList.txt
 #find | grep .WAV | xargs cp -t ~/Music/zoom/script
 cd ~/Music/zoom/script
 rename 'y/A-Z/a-z/' *
